91c81f3f95c7bfcbbb2d1a6e3033d5fd75317bcf
gideonfrans22 <gideonfrans22@gmail.com>
2022-06-27 10:36:52 +0700

    seo for product page

--- pages/produk/index.js
+++ pages/produk/index.js
@@ -2,8 +2,9 @@ import { clientAxios, baseURL } from "../../client";
 import ProductList from "components/ProductList";
 import Link from "next/link";
 import { useState } from "react";
+import { NextSeo } from "next-seo";
 
-const index = ({ page, products, categories }) => {
+const index = ({ page, products, categories, SEO }) => {
   const [filterProduct, setFilterProduct] = useState([]);
 
   const searchProduct = (search) => {
@@ -19,60 +20,65 @@ const index = ({ page, products, categories }) => {
   };
 
   return (
-    <main>
-      <div className="jumbotron jumbotron-fluid mb-0 produk-page">
-        <div className="container">
-          <h1>{page.judul_halaman}</h1>
-          <p className="lead">{page.deskripsi_halaman}</p>
-        </div>
-      </div>
-
-      <div className="container py-5">
-        <h3>Cari Produk:</h3>
-        <form onClick={handleSubmit}>
-          <label class="sr-only" for="search">
-            Cari Produk
-          </label>
-          <div className="d-flex mb-4">
-            <div class="input-group">
-              <input
-                type="text"
-                class="form-control"
-                id="search"
-                placeholder="150 Medical Oxygen"
-                onChange={(e) => searchProduct(e.target.value)}
-              />
-              <button className="btn btn-primary">Cari</button>
-            </div>
+    <>
+      <NextSeo {...SEO} />
+      <main>
+        <div className="jumbotron jumbotron-fluid mb-0 produk-page">
+          <div className="container">
+            <h1>{page.judul_halaman}</h1>
+            <p className="lead">{page.deskripsi_halaman}</p>
           </div>
-        </form>
-        <div className="mb-4">
-          <h4>Kategori Produk</h4>
-          {categories.map((category) => {
-            return (
-              <Link
-                href="/produk/kategori/[id]"
-                as={`/produk/kategori/${category.slug}`}
-                key={category.id}
-              >
-                <a>
-                  <h4 className="badge badge-warning mr-1">{category.nama}</h4>
-                </a>
-              </Link>
-            );
-          })}
         </div>
-        <div className="row">
-          {filterProduct.length
-            ? filterProduct.map((product) => {
-                return <ProductList key={product.id} product={product} />;
-              })
-            : products.map((product) => {
-                return <ProductList key={product.id} product={product} />;
-              })}
+
+        <div className="container py-5">
+          <h3>Cari Produk:</h3>
+          <form onClick={handleSubmit}>
+            <label class="sr-only" for="search">
+              Cari Produk
+            </label>
+            <div className="d-flex mb-4">
+              <div class="input-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="search"
+                  placeholder="150 Medical Oxygen"
+                  onChange={(e) => searchProduct(e.target.value)}
+                />
+                <button className="btn btn-primary">Cari</button>
+              </div>
+            </div>
+          </form>
+          <div className="mb-4">
+            <h4>Kategori Produk</h4>
+            {categories.map((category) => {
+              return (
+                <Link
+                  href="/produk/kategori/[id]"
+                  as={`/produk/kategori/${category.slug}`}
+                  key={category.id}
+                >
+                  <a>
+                    <h4 className="badge badge-warning mr-1">
+                      {category.nama}
+                    </h4>
+                  </a>
+                </Link>
+              );
+            })}
+          </div>
+          <div className="row">
+            {filterProduct.length
+              ? filterProduct.map((product) => {
+                  return <ProductList key={product.id} product={product} />;
+                })
+              : products.map((product) => {
+                  return <ProductList key={product.id} product={product} />;
+                })}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
@@ -81,11 +87,26 @@ export async function getServerSideProps() {
   const page = await clientAxios("/pages/4");
   const categories = await clientAxios(`/product-categories`);
 
+  const SEO = {
+    title: page?.judul_halaman || "Daftar Alat kesehatan",
+    description:
+      products?.map((product) => product?.nama?.substring(0, 15)).toString() ||
+      "pulse Oximeter, pulse Oxymeter, meja Operasi, Instrumen Bedah, Oxygen generator",
+    canonical: "https://kasihkaruniakekalpt.com/produk",
+    openGraph: {
+      type: "website",
+      locale: "en_IE",
+      url: "https://kasihkaruniakekalpt.com/produk",
+      site_name: "PT Kasih Karunia Kekal",
+    },
+  };
+
   return {
     props: {
       products: products.data,
       page: page.data,
       categories: categories.data,
+      SEO: SEO,
     },
   };
 }
