67b2c89e16b43a595b1e859aa1686375ee38e485
gideonfrans22 <gideonfrans22@gmail.com>
2022-06-27 10:52:17 +0700

    seo untuk profil

--- pages/profil.js
+++ pages/profil.js
@@ -1,126 +1,127 @@
-import { clientAxios, baseURL } from "../client";
-import ProductList from "components/ProductList";
-import Link from "next/link";
+import { baseURL, clientAxios } from "../client";
 
-export default function Profil({ page, offices }) {
+export default function Profil({ page, offices, SEO }) {
   return (
-    <main>
-      <div className="jumbotron jumbotron-fluid mb-0 profil-page">
-        <div className="container">
-          <h1>{page.judul_halaman}</h1>
+    <>
+      <NextSeo {...SEO} />
+      <main>
+        <div className="jumbotron jumbotron-fluid mb-0 profil-page">
+          <div className="container">
+            <h1>{page.judul_halaman}</h1>
+          </div>
         </div>
-      </div>
 
-      <div className="container">
-        <blockquote className="blockquote text-center py-5">
-          <i className="fas fa-quote-left h3 mb-4"></i>
-          <p className="mb-0">{page.deskripsi_halaman}</p>
-          <footer className="blockquote-footer">
-            Direktur PT Kasih Karunia Kekal{" "}
-            <cite title="Source Title">Frans Limantoni</cite>
-          </footer>
-        </blockquote>
-      </div>
+        <div className="container">
+          <blockquote className="blockquote text-center py-5">
+            <i className="fas fa-quote-left h3 mb-4"></i>
+            <p className="mb-0">{page.deskripsi_halaman}</p>
+            <footer className="blockquote-footer">
+              Direktur PT Kasih Karunia Kekal{" "}
+              <cite title="Source Title">Frans Limantoni</cite>
+            </footer>
+          </blockquote>
+        </div>
 
-      <section>
-        <div className="container my-4">
-          <div className="row">
-            <div className="col-md-12">
-              <h3 className="mb-4">Office Kami</h3>
-              <div className="row">
-                {offices.map((office) => {
-                  return (
-                    <div className="col-md-4 mb-4" key={office.id}>
-                      <div
-                        id={`carousel${office.id}`}
-                        className="carousel slide"
-                        data-ride="carousel"
-                      >
-                        <div className="carousel-inner">
-                          {office.galeri.map((galeri, idx) => {
-                            return (
-                              <div
-                                className={
-                                  idx == 0
-                                    ? "carousel-item active"
-                                    : "carousel-item"
-                                }
-                                key={galeri.id}
-                              >
-                                <img
-                                  src={baseURL + galeri.url}
-                                  className="d-block w-100 square-img"
-                                  alt={galeri.name}
-                                />
-                                <div className="carousel-caption d-none d-md-block">
-                                  <h5>{office.kota}</h5>
+        <section>
+          <div className="container my-4">
+            <div className="row">
+              <div className="col-md-12">
+                <h3 className="mb-4">Office Kami</h3>
+                <div className="row">
+                  {offices.map((office) => {
+                    return (
+                      <div className="col-md-4 mb-4" key={office.id}>
+                        <div
+                          id={`carousel${office.id}`}
+                          className="carousel slide"
+                          data-ride="carousel"
+                        >
+                          <div className="carousel-inner">
+                            {office.galeri.map((galeri, idx) => {
+                              return (
+                                <div
+                                  className={
+                                    idx == 0
+                                      ? "carousel-item active"
+                                      : "carousel-item"
+                                  }
+                                  key={galeri.id}
+                                >
+                                  <img
+                                    src={baseURL + galeri.url}
+                                    className="d-block w-100 square-img"
+                                    alt={galeri.name}
+                                  />
+                                  <div className="carousel-caption d-none d-md-block">
+                                    <h5>{office.kota}</h5>
+                                  </div>
                                 </div>
-                              </div>
-                            );
-                          })}
+                              );
+                            })}
+                          </div>
+                          <a
+                            class="carousel-control-prev"
+                            href={`#carousel${office.id}`}
+                            role="button"
+                            data-slide="prev"
+                          >
+                            <span
+                              class="carousel-control-prev-icon"
+                              aria-hidden="true"
+                            ></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a
+                            class="carousel-control-next"
+                            href={`#carousel${office.id}`}
+                            role="button"
+                            data-slide="next"
+                          >
+                            <span
+                              class="carousel-control-next-icon"
+                              aria-hidden="true"
+                            ></span>
+                            <span class="sr-only">Next</span>
+                          </a>
                         </div>
-                        <a
-                          class="carousel-control-prev"
-                          href={`#carousel${office.id}`}
-                          role="button"
-                          data-slide="prev"
-                        >
-                          <span
-                            class="carousel-control-prev-icon"
-                            aria-hidden="true"
-                          ></span>
-                          <span class="sr-only">Previous</span>
-                        </a>
-                        <a
-                          class="carousel-control-next"
-                          href={`#carousel${office.id}`}
-                          role="button"
-                          data-slide="next"
-                        >
-                          <span
-                            class="carousel-control-next-icon"
-                            aria-hidden="true"
-                          ></span>
-                          <span class="sr-only">Next</span>
-                        </a>
+                        <ul className="list-group list-group">
+                          <li className="list-group-item">
+                            <i className="fas fa-map-marker-alt mr-4"></i>
+                            {office.alamat}
+                          </li>
+                          <li className="list-group-item">
+                            <i className="fas fa-city mr-4"></i>
+                            {office.kota}
+                          </li>
+                          <li className="list-group-item">
+                            <i className="fas fa-mail-bulk mr-4"></i>
+                            {office.kode_pos}
+                          </li>
+
+                          {office.telepon ? (
+                            <li className="list-group-item">
+                              <i className="fas fa-phone mr-4"></i>
+                              {office.telepon}
+                            </li>
+                          ) : null}
+
+                          {office.fax ? (
+                            <li className="list-group-item">
+                              <i className="fas fa-fax mr-4"></i>
+                              {office.fax}
+                            </li>
+                          ) : null}
+                        </ul>
                       </div>
-                      <ul className="list-group list-group">
-                        <li className="list-group-item">
-                          <i className="fas fa-map-marker-alt mr-4"></i>
-                          {office.alamat}
-                        </li>
-                        <li className="list-group-item">
-                          <i className="fas fa-city mr-4"></i>
-                          {office.kota}
-                        </li>
-                        <li className="list-group-item">
-                          <i className="fas fa-mail-bulk mr-4"></i>
-                          {office.kode_pos}
-                        </li>
-
-                        {office.telepon ? (
-                          <li className="list-group-item">
-                            <i className="fas fa-phone mr-4"></i>
-                            {office.telepon}
-                          </li>
-                        ) : null}
-
-                        {office.fax ? (
-                          <li className="list-group-item">
-                            <i className="fas fa-fax mr-4"></i>
-                            {office.fax}
-                          </li>
-                        ) : null}
-                      </ul>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   );
 }
 
@@ -128,10 +129,28 @@ export async function getServerSideProps() {
   const offices = await clientAxios("/offices?_sort=created_at:DESC");
   const page = await clientAxios("/pages?halaman=profil");
 
+  const deskripsi = `${
+    page.data[0]?.deskripsi_halaman
+  } - Direktur PT Kasih Karunia Kekal Frans Limantoni. Office Kami: ${offices.data
+    ?.map((office) => office.kota)
+    .toString()}`;
+  const SEO = {
+    title: "Profil",
+    description: deskripsi || "Berdiri sejak 1999",
+    canonical: "https://kasihkaruniakekalpt.com/profil",
+    openGraph: {
+      type: "website",
+      locale: "en_IE",
+      url: "https://kasihkaruniakekalpt.com/profil",
+      site_name: "PT Kasih Karunia Kekal",
+    },
+  };
+
   return {
     props: {
       page: page.data[0],
       offices: offices.data,
+      SEO,
     },
   };
 }
